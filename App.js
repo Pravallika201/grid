f7c62cfa4c1058792b8afae6c11deffc1509a349
Pravallika201 <pravalli.201@gmail.com>
2024-06-24 10:08:23 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Students from './Students';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Students Data</h1>
+      </header>
+      <Students/>
+    </div>
+  );
+}
+
+export default App;
